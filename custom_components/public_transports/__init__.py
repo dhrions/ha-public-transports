57f798c8c4056ffbebda8712348fe387802673bd
dhrions <aturing@laposte.net>
2024-08-11 22:52:39 +0200

    🐛 Add method to avoid some debug logs

--- custom_components/public_transports/__init__.py
+++ custom_components/public_transports/__init__.py
@@ -1,7 +1,21 @@
-from homeassistant import core
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
 
-
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the public_transports component."""
     # @TODO: Add setup code.
     return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
+    """Set up Public Transports from a config entry."""
+    hass.data.setdefault("public_transports", {})
+
+    # Vous pouvez stocker les données de l'entrée de configuration dans les données de l'intégration
+    hass.data["public_transports"][entry.entry_id] = entry.data
+
+    # Effectuez ici toute initialisation nécessaire pour votre intégration
+    # Par exemple, créer des entités ou configurer des services
+    # ...
+
+    return True
+
